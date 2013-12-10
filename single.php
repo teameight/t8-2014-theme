d9a3673a8944d11c2b3eb9bac56496e07fdf14ba
AndrewMowe <andrew@team-eight.com>
2013-12-09 16:31:45 -0800

    undid last commit, oops

--- single.php
+++ single.php
@@ -14,11 +14,13 @@ get_header(); ?>
 
                 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                     <div class="phead cf">
-                        <header class="article-header tcrd cf">
-                            <h1><?php the_title(); ?></h1>
-                            <?php if( get_field('subtitle') ): ?>
-                                <h5 class="subheading"><?php the_field('subtitle'); ?></h5>
-                            <?php endif; ?>
+                        <header class="article-header tcrd">
+                            <div class="cf">
+                                <h1><?php the_title(); ?></h1>
+                                <?php if( get_field('subtitle') ): ?>
+                                    <h5 class="subheading"><?php the_field('subtitle'); ?></h5>
+                                <?php endif; ?>
+                            </div>
                         </header>
                         <div class="pcont entry-content">
                             <div class="text">

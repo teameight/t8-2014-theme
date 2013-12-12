79cf528e107bf26d6bf4bf877f24717e0165008f
spencer8 <spencer@team-eight.com>
2013-12-12 17:46:35 -0500

    removes the search field from 404 page

--- 404.php
+++ 404.php
@@ -19,9 +19,8 @@ get_header(); ?>
 			<div class="page-wrapper">
 				<div class="page-content">
 					<h2><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'twentythirteen' ); ?></h2>
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentythirteen' ); ?></p>
+					<p><?php _e( 'It looks like nothing was found at this location.', 'teameight' ); ?></p>
 
-					<?php get_search_form(); ?>
 				</div><!-- .page-content -->
 			</div><!-- .page-wrapper -->
 

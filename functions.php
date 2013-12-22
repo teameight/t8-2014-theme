254df70aad4609cab597fc2ff60a7a2b298a443d
spencer8 <spencer@team-eight.com>
2013-12-22 15:17:35 -0500

    Settles on best case pre_get_posts w/ sticky crap, renames include_category func to team_eight_query_other_things, removed some partial localization support, edited some commenting

--- functions.php
+++ functions.php
@@ -1,14 +1,5 @@
 <?php
 /**
- * Team Eight functions and definitions
- *
- * Sets up the theme and provides some helper functions, which are used in the
- * theme as custom template tags. Others are attached to action and filter
- * hooks in WordPress to change core functionality.
- *
- */
-
-/**
  * Team Eight setup.
  *
  * Sets up theme defaults and registers the various WordPress features 
@@ -16,7 +7,6 @@
  * @uses add_theme_support() To add support for post
  * formats, and post thumbnails.
  * @uses register_nav_menu() To add support for a navigation menu.
- * @uses set_post_thumbnail_size() To set a custom post thumbnail size.
  *
  * @return void
  */
@@ -31,7 +21,7 @@ function teameight_setup() {
 	) );
 
 	// This theme uses wp_nav_menu() in one location.
-	register_nav_menu( 'primary', __( 'Navigation Menu', 'teameight' ) );
+	register_nav_menu( 'primary', 'Navigation Menu' );
 
 	/*
 	 * This theme uses a custom image size for featured images, displayed on
@@ -47,10 +37,7 @@ add_action( 'after_setup_theme', 'teameight_setup' );
  * @return void
  */
 function teameight_scripts_styles() {
-/*
-	// Add Genericons font, used in the main stylesheet.
-	wp_enqueue_style( 'genericons', get_template_directory_uri() . '/fonts/genericons.css', array(), '2.09' );
-	 */
+
 	// Loads JavaScript file with functionality specific to Team Eight.
 	wp_enqueue_script( 'teameight-script', get_template_directory_uri() . '/js/functions.js', array( 'jquery' ), '2013-07-18', true );
 
@@ -66,12 +53,38 @@ add_action( 'wp_enqueue_scripts', 'teameight_scripts_styles' );
 /**
  * Alter the main query on the home page
  *
- * limits the main query to the 9 most recent posts in the home category
+ * limits the main query to the 9 most recent posts in the other-things category
  */
-function include_category( $query ) {
+function team_eight_query_other_things( $query ) {
     if ( $query->is_home() && $query->is_main_query() ) {
-        $query->set( 'category_name', 'other-things' );
-        $query->set('posts_per_page', '9');
+        $query->set( 'cat', 48 ); // other things cat id
+        // set the number of posts per page
+        $posts_per_page = 9;
+        // get sticky posts array
+        $sticky_posts = get_option( 'sticky_posts' );
+
+        // if we have any sticky posts and we are at the first page
+        if (is_array($sticky_posts)) {
+
+            // count the number of sticky posts
+            $sticky_count = count($sticky_posts);
+
+            // and if the number of sticky posts is less than
+            // the number we want to set:
+            if ($sticky_count < $posts_per_page) {
+                $query->set('posts_per_page', $posts_per_page - $sticky_count);
+
+                // if the number of sticky posts is greater than or equal
+                // the number of pages we want to set, ignore sticky posts
+            } else {
+                $query->set('posts_per_page', 1);
+            }
+
+            // fallback in case we have no sticky posts
+            // and we are not on the first page
+        } else {
+            $query->set('posts_per_page', $posts_per_page);
+        }
     }
 }
-add_action( 'pre_get_posts', 'include_category' );
+add_action( 'pre_get_posts', 'team_eight_query_other_things' );
